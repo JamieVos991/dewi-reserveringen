15d5086131c0230e056d970bbcbc8396371a5c8d
Jamie Vos <Jamievos100@gmail.com>
2026-01-15 14:36:37 +0100

    dsahboard css verbeterd

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -13,8 +13,8 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-firestore.js";
 
 // UI elementen
-const container = document.getElementById("reservationsContainer");
-const logoutBtn = document.getElementById("logoutBtn");
+const container = document.getElementById("reservaties-container");
+const logoutBtn = document.getElementById("loguit-knop");
 const studioFilter = document.getElementById("studioFilter");
 
 // Pagination 
@@ -93,21 +93,29 @@ function renderReservations() {
 
   // Genereer HTML voor elk item
   container.innerHTML = pageItems.map(r => `
-    <article>
-      <section>
-        <p>${r.email}</p>
-
+    <details>
+    <summary>${r.email} 
         <svg
-          data-id="${r.id}"
+  width="20"
+     data-id="${r.id}"
           class="deleteBtn"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17Z" fill="currentColor" />
-          <path d="M9 9H11V17H9V9Z" fill="currentColor" />
-          <path d="M13 9H15V17H13V9Z" fill="currentColor" />
-        </svg>
+  height="20"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z"
+    fill="currentColor"
+  />
+  <path d="M9 9H11V17H9V9Z" fill="currentColor" />
+  <path d="M13 9H15V17H13V9Z" fill="currentColor" />
+</svg>
+        </summary>
+      <section>
+       
       </section>
 
       <section>
@@ -119,7 +127,7 @@ function renderReservations() {
         <p>${r.startTijd} - ${r.eindTijd}</p>
         <p>${r.datum}</p>
       </section>
-    </article>
+    </details>
   `).join("");
 }
 
